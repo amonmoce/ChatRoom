3a1b2ca75b113eb27026dea985c00540e8d26908
Amon <amonmoce@yahoo.fr>
2015-10-28 18:07:51 +0800

    Add back lib folder ... looks like that there is a git bug ... or I didnt understand why the files inside lib dont appear

---
+++ lib/common.h
@@ -0,0 +1,14 @@
+#ifndef  	COMMON_H_
+#define		COMMON_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+#define DEFAULT_SERVER_PORT 7777
+#define MESSAGE_MAX_SIZE 2048
+
+#endif
